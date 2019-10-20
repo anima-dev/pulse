141573b06bf8279ab4d70db72a11822984d4e6d9
Anna <an.gvozdikova@gmail.com>
2019-10-20 11:29:20 +0400

    corrected urls

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
     $('.carousel__inner').slick({
         speed: 1200,
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/right.svg"></button>',
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.svg"></button>',
         responsive: [{
             breakpoint: 768,
             settings: {
