f85315d0d0245f272da772fdfe7ebc3d10a9cce7
Anna <an.gvozdikova@gmail.com>
2019-10-27 14:03:17 +0400

    added modal windows

--- src/js/script.js
+++ src/js/script.js
@@ -31,4 +31,20 @@ $(document).ready(function() {
     toggleSlide('.catalog__details-link');
     toggleSlide('.catalog__back-link');
 
+    // Modal
+
+    $('[data-modal=consultation]').on('click', function() {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click', function() {
+        $('.overlay, #consultation, #order, #thanks').fadeOut('slow');
+    });
+
+
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalog__title').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        })
+    });
 });
